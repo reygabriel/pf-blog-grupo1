331a2625bd095ead40a820832e167e8e22fd3d55
Claudio Lopez <claudio21596@gmail.com>
2023-12-16 23:43:06 -0300

    Se creo la view para crear publicaciones

--- src/publicaciones/urls.py
+++ src/publicaciones/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns=[
-    path('ver-publicaciones/', views.PublicacionesView.as_view(), name='publicaciones')
+    path('ver-publicaciones/', views.PublicacionesView.as_view(), name='publicaciones'),
+    path('publicar/', views.Publicar.as_view(), name='publicar')
 ]
--- src/publicaciones/views.py
+++ src/publicaciones/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Publicacion
-from django.views.generic import ListView
-
+from django.views.generic import ListView, CreateView, UpdateView
+from .forms import PublicarForm
 
 class PublicacionesView(ListView):
-    template_name = 'publicaciones.html'
+    template_name = 'publicaciones/publicaciones.html'
     model = Publicacion
     context_object_name = 'publicaciones'
 
 
-
+class Publicar(CreateView):
+    model = Publicacion
+    template_name = 'publicaciones/publicar.html'
+    form_class = PublicarForm
