8b6a8cb49538da533648fbb51d8e4e1cf36bb192
Bruno De Bonis <brunome@hotmail.com>
2023-12-17 21:51:59 -0300

    Modificar y Eliminar Publicaciones

--- src/publicaciones/urls.py
+++ src/publicaciones/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns=[
     path('ver-publicaciones/', views.PublicacionesView.as_view(), name='publicaciones'),
-    path('publicar/', views.Publicar.as_view(), name='publicar')
+    path('publicar/', views.Publicar.as_view(), name='publicar'),
+    path('modificar/<int:pk>', views.ModificarPublicacionView.as_view(), name='modificar-publicacion'),
+    path('eliminar/<int:pk>', views.EliminarPublicacionView.as_view(), name= 'eliminar-publicacion')
 ]
