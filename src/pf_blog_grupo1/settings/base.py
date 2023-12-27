921bf2a1c5a041af9a7c365d7198947df00d04ba
Gabriel Rey <rgabi1219@gmail.com>
2023-12-27 20:52:05 -0300

    Encuentro el error y queda funcionando en local ok

--- src/pf_blog_grupo1/settings/base.py
+++ src/pf_blog_grupo1/settings/base.py
@@ -9,7 +9,7 @@ AUTH_USER_MODEL = 'usuarios.Usuario'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!#*w+$yy-)3q7yn65eaex@ul#ikmy$6=sjau6$n@dvwzp&aoh3'
 
-ALLOWED_HOSTS = ['reygabriel95.pythonanywhere.com'],
+ALLOWED_HOSTS = ['reygabriel95.pythonanywhere.com','127.0.0.1']
 
 # Variables relacionadas al login
 LOGIN_URL = '/usuarios/login/'
